0bedab230063f243823c429749b95933552b3cb8
Maite Rosalie <maite.rosalie@gmail.com>
2020-03-30 19:26:08 -0300

    [ref]Remove unnecesary layout component

--- components/Layout.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactNode } from 'react';
-
-export const Layout = ({ children }: { children: ReactNode }) => (
-  <div className="page">{children}</div>
-);
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
-export * from './Layout';
 export * from './List';
 export * from './ListItem';
 export * from './Modal';
